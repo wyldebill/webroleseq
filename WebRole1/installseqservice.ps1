695ec8163e034eda9eced5b1240ad7e04a2962dc
william austin <william.austin@solutiondesign.com>
2015-06-26 10:26:44 -0500

    initial commit

---
+++ WebRole1/installseqservice.ps1
@@ -0,0 +1,24 @@
+﻿Start-Transcript
+$url = "https://getseq.net/Download/Begin?version=1.6.12"
+$path = "E:\approot\bin\seq.msi"
+$args = "/passive"
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$webClient = new-object System.Net.WebClient
+$webClient.DownloadFile( $url, $path )  >> downloadlog.txt
+
+Start-Process `
+    -file $path `
+    -arg $args 
+
+$seqlinstalldir = "D:\program files\seq\seq.exe"
+$seqinstallcmd = "install"
+$seqstartcmd = "start"
+
+Start-Process `
+    -file $seqlinstalldir `
+    -arg $seqinstallcmd
+
+Start-Process `
+	-file $seqlinstalldir `
+	-arg $seqstartcmd
+Stop-Transcript
---
+++ WebRole1/startup.cmd
@@ -0,0 +1,2 @@
+powershell -c "set-executionpolicy unrestricted"
+powershell -c ".\installseqservice.ps1 >> powershell.txt"
